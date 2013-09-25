cc37ef1d6aba0c435773d62135ffda6aee2ecc46
Max Bauer <mbauer7532@gmail.com>
2013-09-25 22:39:53 +0300

    Project checkin.

---
+++ src/eagleml/EagleML.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eagleml;
+
+/**
+ *
+ * @author Neo
+ */
+public class EagleML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Testing checkins...
+        // 
+    }
+}
