6e53a3e404117fd321be77339729e97e173ee8cd
Max Bauer <mbauer7532@gmail.com>
2013-09-27 23:39:39 +0300

    1. Create the AST datatype. 2. Move the main function out of the parser and into EagleML.java. 3. Adjust the parser for the new location of the AST datatype. 4. Get rid of some javacc warnings by introducing a lookahead. 5. Implement the toString() function for operators.

--- src/eagleml/EagleML.java
+++ src/eagleml/EagleML.java
@@ -14,7 +14,30 @@ public class EagleML {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Testing checkins...
-        // yet more testing.
+      EagleMLParser parser;
+      if (args.length == 0) {
+        System.out.println("Java Parser Version 1.0.2:  Reading from standard input . . .");
+        parser = new EagleMLParser(System.in);
+      } else if (args.length == 1) {
+        System.out.println("Java Parser Version 1.0.2:  Reading from file " + args[0] + " . . .");
+        try {
+          parser = new EagleMLParser(new java.io.FileInputStream(args[0]));
+        } catch (java.io.FileNotFoundException e) {
+          System.out.println("Java Parser Version 1.0.2:  File " + args[0] + " not found.");
+          return;
+        }
+      } else {
+        System.out.println("Java Parser Version 1.0.2:  Usage is one of:");
+        System.out.println("         java EagleMLParser < inputfile");
+        System.out.println("OR");
+        System.out.println("         java EagleMLParser inputfile");
+        return;
+      }
+      try {
+        parser.CompilationUnit();
+        System.out.println("Java Parser Version 1.0.2:  Java program parsed successfully.");
+      } catch (ParseException e) {
+        System.out.println("Java Parser Version 1.0.2:  Encountered errors during parse.");
+      }
     }
 }
---
+++ src/eagleml/EagleMLAst.java
@@ -0,0 +1,126 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eagleml;
+
+import java.util.List;
+
+/**
+ *
+ * @author Neo
+ */
+final public class EagleMLAst {
+  static public enum OperAssociativity {
+    LEFT,
+    RIGHT,
+    NOASSOC
+  };
+
+  static public enum Operator {
+    OR      (0, OperAssociativity.LEFT),
+    AND     (1, OperAssociativity.LEFT),
+
+    LT      (2, OperAssociativity.NOASSOC),
+    GT      (2, OperAssociativity.NOASSOC),
+    LEQ     (2, OperAssociativity.NOASSOC),
+    GEQ     (2, OperAssociativity.NOASSOC),
+    EQ      (2, OperAssociativity.NOASSOC),
+    NEQ     (2, OperAssociativity.NOASSOC),
+
+    PLUS    (3, OperAssociativity.LEFT),
+    MINUS   (3, OperAssociativity.LEFT),
+    TIMES   (4, OperAssociativity.LEFT),
+    DIVIDE  (4, OperAssociativity.LEFT),
+    REM     (4, OperAssociativity.LEFT),
+    XOR     (5, OperAssociativity.LEFT);
+
+    final private int m_precedence;
+    final private OperAssociativity m_associativity;
+
+    private Operator(final int precedence, final OperAssociativity associativity) {
+      m_precedence = precedence;
+      m_associativity = associativity;
+    }
+
+    public int getPrecedence() { return m_precedence; }
+    public OperAssociativity getAssociativity() { return m_associativity; }
+
+    @Override
+    final public String toString()
+    {
+      switch (this)
+      {
+      case OR:     return "||";
+      case AND:    return "&&";
+
+      case LT:     return "<";
+      case GT:     return ">";
+      case LEQ:    return "<=";
+      case GEQ:    return ">=";
+      case EQ:     return "==";
+      case NEQ:    return "!=";
+
+      case PLUS:   return "+";
+      case MINUS:  return "-";
+      case TIMES:  return "*";
+      case DIVIDE: return "/";
+      case REM:    return "%";
+      case XOR:    return "^";
+      }
+      }
+    }
+  };
+
+  static public class EagleMLType {}
+
+  final static public class TypedVar {
+    static public TypedVar create(String name, EagleMLType typ) {
+      return new TypedVar();
+    }
+  }
+
+  static public EagleMLType makeIntType() { return new EagleMLType(); }
+  static public EagleMLType makeBoolType() { return new EagleMLType(); }
+
+  static public class ExprAst {};
+
+  final static public class IntLit extends ExprAst {
+    public static IntLit create(String intStr) {
+      return null;
+    }
+  };
+
+  final static public class BoolLit extends ExprAst {
+    public static BoolLit create(boolean b) {
+      return null;
+    }
+  };
+
+  final static public class VarRef extends ExprAst {
+    static public VarRef create(String varName) {
+      return null;
+    }
+  };
+
+  final static public class BinOper extends ExprAst {
+    static public BinOper create(ExprAst t1, Operator oper, ExprAst t2) {
+      return null;
+    }
+  };
+
+  static public class Def {};
+
+  final static public class FunDef extends Def
+  {
+    static public FunDef create(String funName, List<TypedVar> tyVars, EagleMLType funType, ExprAst expr) {
+      return new FunDef();
+    }
+  };
+
+  final static public class VarDef extends Def {
+    static public VarDef create() {
+      return null;
+    }
+  };
+}
