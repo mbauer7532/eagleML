013ad16ca0524bd8b9c7afa5a6b46faebb4b94a1
Max Bauer <mbauer7532@gmail.com>
2013-10-05 00:04:02 +0300

    The skeleton of the type-checking module.

--- src/eagleml/typeChecker/TypeChecker.java
+++ src/eagleml/typeChecker/TypeChecker.java
@@ -6,12 +6,97 @@
 
 package eagleml.typeChecker;
 
+import eagleml.ast.AstVisitor;
 import eagleml.ast.EagleMLAst.*;
+import eagleml.ast.EagleMLTypes;
 
 /**
  *
  * @author Neo
  */
-public class TypeChecker {
+public class TypeChecker implements AstVisitor {
+  @Override
+  public void visit(final DefinitionList defList) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 
+  @Override
+  public void visit(final FunDef funDef) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final VarDef varDef) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final TypedVarList typedVarList) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final TypedVar typedVar) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final ExprList exprList) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final IntLit intLit) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final BoolLit boolLit) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final VarRef varRef) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final UnaryOper uOper) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final BinOper binOper) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final IfExpr binOper) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final LetExpr binOper) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final FunCall binOper) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final EagleMLTypes.IntPrimitiveType intTyp) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final EagleMLTypes.BoolPrimitiveType boolTyp) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void visit(final EagleMLTypes.FunType boolTyp) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 }
